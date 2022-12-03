46656f215de1750988dff124d3b4dd6c9c70ae6d
Quadadulahma <henry.koeser@gmail.com>
2022-12-04 00:30:28 +0100

    Projektdateien hinzufügen.

---
+++ makeTrym/makeTrym.cpp
@@ -0,0 +1,19 @@
+﻿#include "makeTrym.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::cout << std::endl;
+
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(500,500,"hasoidj",0,0);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ makeTrym/makeTrym.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+#include <iostream>
+#include "GLFW/glfw3.h"
